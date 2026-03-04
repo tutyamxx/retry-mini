3269f76e0c687e1b71a5d72796a47a105deb44ae
tutyamxx <15928925+tutyamxx@users.noreply.github.com>
2026-03-04 18:07:47 +0000

    UPDATE: Fixed dependencies

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  *  retry-mini - ↻ Tiny dependency-free async retry utility with backoff, delay, and conditional retry support.
- *  @version: v1.1.0
+ *  @version: v1.1.1
  *  @link: https://github.com/tutyamxx/retry-mini
  *  @license: MIT
  **/
